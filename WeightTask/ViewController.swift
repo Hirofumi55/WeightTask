e305325f721c97f5653ab63191f523962597e25a
Sasakura Hirofumi <hirofumi@MacBook.local>
2017-06-11 15:56:52 +0900

    New

---
+++ WeightTask/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WeightTask
+//
+//  Created by Sasakura Hirofumi on 2017/06/11.
+//  Copyright © 2017 Sasakura Hirofumi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
