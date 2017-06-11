40ce619bbea57fee3356cc1ccef5d642e964bdd9
Sasakura Hirofumi <hirofumi@MacBook.local>
2017-06-11 16:06:07 +0900

    WightData.classの追加

---
+++ WeightTask/WeightData.swift
@@ -0,0 +1,27 @@
+//
+//  WeightData.swift
+//  WeightTask
+//
+//  Created by Sasakura Hirofumi on 2017/06/11.
+//  Copyright © 2017 Sasakura Hirofumi. All rights reserved.
+//
+
+import Foundation
+
+class WeightData {
+    
+    
+    //メニュー名
+    var name: String? = ""
+    
+    //Rep数
+     var rep: Int = 0
+    
+    //Set数
+    var set: Int = 0
+    
+    //check機能
+    var check: Bool = false
+    
+    
+}
